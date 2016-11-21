7d7442dff8b3a829da0f9cedaa8886bf0be387f1
Bassam Tabbara <bassam.tabbara@quantum.com>
2016-11-20 20:14:04 -0800

    embedded: add RGW to libcephd
    added RGW and rgw-admin to libcephd.
    
    Signed-off-by: Bassam Tabbara <bassam.tabbara@quantum.com>

--- src/rgw/rgw_main.cc
+++ src/rgw/rgw_main.cc
@@ -100,7 +100,7 @@ static void wait_shutdown()
   }
 }
 
-int signal_fd_init()
+static int signal_fd_init()
 {
   return socketpair(AF_UNIX, SOCK_STREAM, 0, signal_fd);
 }
@@ -157,7 +157,7 @@ public:
   }
 };
 
-int usage()
+static int usage()
 {
   cerr << "usage: radosgw [options...]" << std::endl;
   cerr << "options:\n";
@@ -195,7 +195,11 @@ static void reloader_handler(int signum)
 /*
  * start up the RADOS connection and then handle HTTP messages as they come in
  */
+#ifdef BUILDING_FOR_EMBEDDED
+extern "C" int cephd_rgw(int argc, const char **argv)
+#else
 int main(int argc, const char **argv)
+#endif
 {
   // dout() messages will be sent to stderr, but FCGX wants messages on stdout
   // Redirect stderr to stdout.
